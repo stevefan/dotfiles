5a464e99934586e65f3050291541cb9b5ffb088d
Steven Fan <stevencfan@gmail.com>
2017-07-17 12:29:45 -0700

    used a found, get-scripts-directory line to determine SCRIPTPATH

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,28 +1,7 @@
 #!/usr/bin/env bash
 
-#make script do different things in different contexts,
-#curl install & install from git
-#passing on curl+install for git clone & source install-dotfiles.sh
-#determine the context of installation
-#determine which github based on ubuntu email?
-
-#philosophy & aim
-#easy deploy through salt state apply
-#easy manual deploy by hand
-#linux only, maybe as portable within reason (e.g. post 2014 platforms?)
-#idemipotent install
-#install overrides previous symlinks
-#maybe save old bashrc and files?
-#
-
-# 'ls -a1' sorted lists all files in the current directory outputting one per line.
-# 'sort excluded-files' sorted lists the lines in the file representative of non-dotfiles responsible for distribution or installation of dotfiles.
-# '<(bash-command)' runs bash-command in a subshell and redirects the output as an input.
-# comm -23 compares inputs line by line and outputs unique lines in the first argument while excluding from output the unique lines in the second output and lines common to both.
-# Together this line gives all non-excluded-files in the current directory.
-# echo another-file >> excluded-files' to exclude additional files.
-
-SCRIPTPATH="${BASH_SOURCE%/*}"
+# via http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EXCLUDED_FILES=$SCRIPTPATH/excluded_files
 
 for dotfile in $(comm -23 <(ls -a1 $SCRIPTPATH) <(sort $EXCLUDED_FILES) )
